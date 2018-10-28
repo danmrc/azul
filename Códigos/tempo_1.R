947c9c810d0d762494e606b20e8cb191ab91ce63
danmrc <36049697+danmrc@users.noreply.github.com>
2018-10-28 17:54:10 -0300

    códigos do Julia vs R

--- Códigos/tempo_1.R
+++ Códigos/tempo_1.R
@@ -1,21 +1,26 @@
-x <- matrix(rnorm(500),ncol = 5)
-bet <- c(1,2,3,4,5)
-y <- x%*%bet+rnorm(100)
+library(benchr)
 
 ols <- function(x,y){
   solve(t(x)%*%x)%*%t(x)%*%y
 }
 
+func <- function(){
+  x <- matrix(rnorm(500),ncol = 5)
+  bet <- c(1,2,3,4,5)
+  y <- x%*%bet+rnorm(100)
+  ols(x,y)
+}
 
-benchmark(ols(x,y))
+aa <- benchmark(func())
 
 benchmark(lm(y ~ x))
+
+#Para a última seção do artigo:
 
 ##2 Otimização
 
 rm(list=ls())
 
-x <- rweibull(500,1)
 
 weib <- function(par){
   -1*sum(log(par[2]) - log(par[1]) + (par[2]-1)*(log(x)-log(par[1]))-(x/par[1])^par[2])
@@ -23,14 +28,19 @@ weib <- function(par){
 
 x0 = c(2,2)
 
-benchmark(optim(x0,weib))
+func2 <- function(){
+  x <- rweibull(500,1)
+  optim(x0,weib,method="L-BFGS-B",lower=c(0,0))
+}
+
+benchmark(func2())
 
 ##3 Bootstrapping
 
 
 
-Boot <- function(amostra){
-
+Boot <- function(){
+  amostra <- rnorm(1000)
   boot <- rep(0,10000)
   
   for(i in 1:10000){
@@ -39,11 +49,5 @@ Boot <- function(amostra){
   }
 }
 
-tempo <- rep(0,100)
 
-for(i in 1:100){
-  amostra <- rnorm(1000)
-  tempo[i] <- system.time(Boot(amostra))[3]
-}
-
-mean(tempo)
+benchmark(Boot())
--- Códigos/tempo_1.jl
+++ Códigos/tempo_1.jl
@@ -4,74 +4,73 @@
 
 ### 1 - MQO
 
+using Statistics
+
 ols(x,y) = inv(x'*x)*x'*y
 
 function func()
-
-    for i = 1:1000
-
-        X = randn(100,5)
-        bet = [1.,2,3,4,5]
-        y = X*bet + randn(100)
-        ols(X,y)
-    end
+    X = randn(100,5)
+    bet = [1.,2,3,4,5]
+    y = X*bet + randn(100)
+    ols(X,y)
 end
 
-tempo = Array{Float64}(undef,100)
+tempo1 = Array{Float64}(undef,100)
 
 for j=1:100
-
-    val, t, bytes, gctime, memallocs =  @timed func()
-    tempo[j] = t
+    tempo1[j] = @elapsed func()
 end
 
-sum(tempo)/length(tempo)
+temp = Statistics.median(tempo1)
 
 ### 2 - Otimização
 
 using Optim
+using Distributions
 
-vals = 1:0.01:10
+weib(par) = -1*sum(log(par[2])-log(par[1]) .+ (par[2]-1) .* (log.(x) .- log(par[1])) .- (x./par[1]) .^par[2])
 
-valores = Array{Float64}(undef,length(vals),1)
-x0=[0 0.]
+g = Weibull(1,1)
 
-for j = 1:length(vals)
-        f(x) = (x[1]-vals[j]).^2 .+ x[2].^2
 
-        val, t, bytes, gctime, memallocs = @timed optimize(f,x0,NelderMead())
-        println(val)
-        valores[j] = t
-    end
+lower = [0 0.]
+upper = [Inf Inf]
+x0 = [2 2.]
 
-sum(valores)/length(valores)
+function func2()
+    x = rand(g,500)
+    optimize(weib,lower,upper,x0,Fminbox(LBFGS()))
+end
+
+tempo2 = Array{Float64}(undef,100)
+
+for i=1:100
+    tempo2[i] = @elapsed optimize(weib,lower,upper,x0,Fminbox(LBFGS()))
+end
+
+temp = Statistics.median(tempo2)
 
 ##3 - Bootstrap
 
-using Distributions
-using Statistics
+function boot()
 
-
-function boot(amostra)
-
+    z = randn(100)
     boot_mean = Array{Float64}(undef,10000)
 
     for i = 1:10000
 
-        prob = repeat([1/length(amostra)],length(amostra))
+        prob = repeat([1/length(z)],length(z))
 
         h = Categorical(prob)
 
-        boot_mean[i] = Statistics.mean(amostra[rand(h,500)])
+        boot_mean[i] = Statistics.mean(z[rand(h,500)])
     end
 end
 
 tempo = Array{Float64}(undef,100)
 
 for j=1:100
-    z = randn(100)
-    val, t, bytes, gctime, memallocs =  @timed boot(z)
-    tempo[j] = t
+    tempo[j]=  @elapsed boot()
 end
 
-sum(tempo)/length(tempo)
+temp3 = Statistics.median(tempo)
