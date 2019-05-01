00ec7d2078b77161631d34e39b942cb6a69fec8c
danmrc <36049697+danmrc@users.noreply.github.com>
2019-05-01 13:46:46 -0300

    post sobre time iteration

--- Códigos/dyn_prog_time_it.jl
+++ Códigos/dyn_prog_time_it.jl
@@ -13,7 +13,7 @@ grid_size = 300
 
 iter = 15
 
-k_grid = range(1e-5,8,length=grid_size)
+k_grid = range(1e-5,8,length=grid_size) #grid para a variável de estado
 
 uline(c) = 1/c
 f(k) = k^alpha
@@ -24,12 +24,12 @@ c_val[:,1] =  f.(k_grid)
 
 for i in 2:iter
     for j in 1:grid_size
-        foo_c = LinearInterpolation(k_grid,c_val[:,i-1],extrapolation_bc = Line())
+        foo_c = LinearInterpolation(k_grid,c_val[:,i-1],extrapolation_bc = Line()) #usando uma aproximação linear (por partes) para a função do consumo
         function g(c)
             y = f(k_grid[j])
             return uline(c) - beta*fline(y-c)*uline(foo_c(y-c))
         end
-        c_val[j,i] = find_zeros(g,1e-10,f(k_grid[j]))[1]
+        c_val[j,i] = find_zeros(g,1e-10,f(k_grid[j]))[1] #resolvendo a equação de euler
     end
 end
 
@@ -38,6 +38,41 @@ maximum(abs.(c_val[:,14]-c_val[:,15]))
 true_pol(k) = (1-alpha*beta)*f(k)
 plot(k_grid,true_pol.(k_grid), lab = "Sol Analítica")
 plot!(k_grid,c_val[:,15], lab = "Sol Numérica" ,line =:dash)
+
+#### Só para comparar, vamos ver o mesmo caso com iteração na função valor e só 15 iterações
+
+using Optim
+
+u(c)=log(c)
+
+y = f.(k_grid)
+
+bet = beta
+alf = alpha
+
+guess = y
+vals = Array{Float64}(undef,150,length(y))
+pol=Array{Float64}(undef,150,length(y))
+
+vals[1,1:length(y)] = guess
+pol[1,1:length(y)] = y
+
+for i=1:15
+  V=LinearInterpolation(y,vals[i,:],extrapolation_bc = Interpolations.Line())
+    for j = 1:length(y)
+        That(c)=-(u(c)+bet*V(f(y[j]-c)))
+        op = optimize(That,0,y[j])
+       vals[(i+1),j]=-Optim.minimum(op)
+       pol[(i+1),j]=Optim.minimizer(op)
+   end
+end
+
+plot(f.(k_grid),true_pol.(k_grid), lab = "Sol Analítica", legend = :topleft)
+plot!(f.(k_grid),c_val[:,7], lab = "Sol Numérica Time Iteration \n" ,line =:dash)
+plot!(f.(k_grid),pol[7,:], lab = "Sol Numérica Value Iteration" ,line =:dash)
+
+plot(f.(k_grid),c_val[:,15] - true_pol.(k_grid), lab = "Erro Time Iteration")
+plot!(f.(k_grid),pol[15,:] - true_pol.(k_grid), lab = "Erro Value Iteration")
 
 ##### Caso em que a economia só tem endowments e esses endowments seguem um processo de Markov
 
