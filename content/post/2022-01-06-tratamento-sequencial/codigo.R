b61038a4069e320de0ae7aca92dc9801ca94c8d6
Daniel Coutinho <36049697+danmrc@users.noreply.github.com>
2022-01-09 12:56:42 -0300

    Atualização no post de tratamento sequencial Política de segurança do Hugo Atualização dos pacotes no renv

--- content/post/2022-01-06-tratamento-sequencial/codigo.R
+++ content/post/2022-01-06-tratamento-sequencial/codigo.R
@@ -2,18 +2,50 @@ simple_rule1 <- function(periodo,cutoff,media0,media1){
   
   if (periodo < cutoff){
     if(periodo %% 2 == 0){
-      return(0)
+      return(c(0,TRUE))
     } else{
-      return(1)
+      return(c(1,TRUE))
     }
     
   } else {
     if(media1 > media0){
-      return(1)
+      return(c(1,FALSE))
     } else{
-      return(0)
+      return(c(0,FALSE))
     }
   }
+}
+
+dgp <- function(prob0,prob1,n0,n1,media0,media1,decision,update){
+  
+  if(decision == 0){
+    
+    suc <- rbinom(1,1,prob0)
+    
+    if(update){
+    
+      media0 <- (n0*media0 + suc)/(n0+1)
+      n0 <- n0 + 1
+    }
+    
+    return(c(suc,n0,n1,media0,media1))
+    
+  } else if(decision == 1){
+    
+    suc <- rbinom(1,1,prob1)
+    
+    if(update){
+      
+      media1 <- (n1*media1 + suc)/(n1+1)
+      n1 <- n1 + 1
+    }
+    
+    return(c(suc,n0,n1,media0,media1))
+    
+  }
+  
+  
+  
 }
 
 simulacao <- function(N,prob0,prob1,rule,...){
@@ -22,27 +54,30 @@ simulacao <- function(N,prob0,prob1,rule,...){
   media1 <- 0
   n0 <- 0
   n1 <- 0
+  payoff <- 0
   
   for(i in 1:N){
     
     decision <- rule(periodo = i, media0 = media0, media1 = media1,...)
     
-    if(decision == 0){
+    if(decision[1] == 0){
       
-      trat <- rbinom(1,1,prob0)
-      n0 <- n0 +1
-      media0 <- ((n0-1)*media0 + trat)/n0
+      result <- dgp(prob0,prob1,n0,n1,media0,media1,decision[1],decision[2])
+      n0 <- result[2]
+      media0 <- result[4]
+      payoff <- result[1] + payoff
       
-    } else if(decision == 1){
+    } else if(decision[1] == 1){
       
-      trat <- rbinom(1,1,prob1)
-      n1 <- n1 +1
-      media1 <- ((n1-1)*media1 + trat)/n1
+      result <- dgp(prob0,prob1,n0,n1,media0,media1,decision[1],decision[2])
+      n1 <- result[3]
+      media1 <- result[5]
+      payoff <- result[1] + payoff
       
     } else{
       stop("Decisão deve ser 0 ou 1")
     }
     
   }
-  return(n0*media0 + n1*media1)
+  return(c(payoff,decision[1]))
 }
